28ad615e0a3ae6091bc7a030b02eb5731f8d3d2f
Seth Bromberger <github@bromberger.com>
2018-03-06 22:27:18 -0800

    need to try newfloatpq

---
+++ priorityqueue/newfloatpq.go
@@ -0,0 +1,54 @@
+package priorityqueue
+
+import (
+	"container/heap"
+)
+
+// An Item is something we manage in a Priority queue.
+type Item struct {
+	Value    uint32  // The Value of the item; arbitrary.
+	Priority float64 // The Priority of the item in the queue.
+	// The index is needed by update and is maintained by the heap.Interface methods.
+	index int // The index of the item in the heap.
+}
+
+// A PriorityQueue implements heap.Interface and holds Items.
+type PriorityQueue []*Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+
+func (pq PriorityQueue) IsEmpty() bool { return pq.Len() == 0 }
+
+func (pq PriorityQueue) Less(i, j int) bool {
+	// We want Pop to give us the highest, not lowest, Priority so we use greater than here.
+	return pq[i].Priority > pq[j].Priority
+}
+
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+	pq[i].index = i
+	pq[j].index = j
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	n := len(*pq)
+	item := x.(*Item)
+	item.index = n
+	*pq = append(*pq, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	item.index = -1 // for safety
+	*pq = old[0 : n-1]
+	return item
+}
+
+// update modifies the Priority and Value of an Item in the queue.
+func (pq *PriorityQueue) update(item *Item, Value uint32, Priority float64) {
+	item.Value = Value
+	item.Priority = Priority
+	heap.Fix(pq, item.index)
+}
